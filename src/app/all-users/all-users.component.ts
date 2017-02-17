6898d2d999da0db0d9f531a5b1a8f0771f7dc2d7
Badreddine Ilhem <badreddine@b11p1.fil.univ-lille1.fr>
2017-02-17 20:00:39 +0100

    recuperation des utilisateurs à tester

--- src/app/all-users/all-users.component.ts
+++ src/app/all-users/all-users.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnexionService } from '../connexion.service';
 
 @Component({
   selector: 'app-all-users',
   templateUrl: './all-users.component.html',
-  styleUrls: ['./all-users.component.css']
+  styleUrls: ['./all-users.component.css'],
+  providers: [ConnexionService]
 })
 export class AllUsersComponent implements OnInit {
 
-  constructor() { }
+  users = "Test";
+  constructor(private logger: ConnexionService) { }
 
   ngOnInit() {
+  }
+
+  allUsers(){
+    this.users = ""+this.logger.getUsers();
   }
 
 }
