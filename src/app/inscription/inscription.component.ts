0d7052e301631fea052a03c628c8081c40c378c9
Badreddine Ilhem <badreddine@b11p1.fil.univ-lille1.fr>
2017-02-17 20:02:17 +0100

    inscription à tester

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ConnexionService } from '../connexion.service';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
-  styleUrls: ['./inscription.component.css']
+  styleUrls: ['./inscription.component.css'],
+  providers: [ConnexionService]
 })
 export class InscriptionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private logger: ConnexionService) { }
 
   ngOnInit() {
+
   }
 
+  logItUp(username, email, passeword){
+    this.logger.logUp(username,email,passeword);
+  }
 }
