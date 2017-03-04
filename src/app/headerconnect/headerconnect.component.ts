326c97db15ff35e7e90b9c9514d40fa75d0116dd
badreilhem <badreddine.ilhem@yahoo.fr>
2017-03-04 21:28:04 +0100

    début vues en mode connecté

---
+++ src/app/headerconnect/headerconnect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headerconnect',
+  templateUrl: './headerconnect.component.html',
+  styleUrls: ['./headerconnect.component.css']
+})
+export class HeaderconnectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/sidebarconnect/sidebarconnect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebarconnect',
+  templateUrl: './sidebarconnect.component.html',
+  styleUrls: ['./sidebarconnect.component.css']
+})
+export class SidebarconnectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
