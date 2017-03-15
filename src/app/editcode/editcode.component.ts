8e07a381aeeeff6b7876bd800370b2798c051c6a
badreilhem <badreddine.ilhem@yahoo.fr>
2017-03-15 22:29:58 +0100

    editer code ok avec langage et tags savecode ok

--- src/app/editcode/editcode.component.ts
+++ src/app/editcode/editcode.component.ts
@@ -2,29 +2,33 @@ import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ConnexionService } from '../connexion.service';
 import { TextareaService } from '../textarea.service';
 import { Router } from '@angular/router';
+import { CodesService } from '../codes.service';
 import { APP_GLOBAL, COOKIE } from '../appglobal';
 
 @Component({
   selector: 'app-editcode',
   templateUrl: './editcode.component.html',
   styleUrls: ['./editcode.component.css'],
-  providers: [ConnexionService, TextareaService]
+  providers: [ConnexionService, TextareaService, CodesService]
 })
 export class EditcodeComponent implements OnInit {
 
   tags=[];
   langage={langage:""};
   codei={code:"", description:""};
-
+  logowts = "./assets/index.png";
   codetoadd = {};
 
   public codeselect="";
   public codeId:number;
   public code;
   public user;
+  public tagspost=[];
+  public langagepost = "";
   constructor(private textarea: TextareaService,
     private router: Router,
-      private logger: ConnexionService) { }
+      private logger: ConnexionService,
+      private codeService: CodesService) { }
 
   ngOnInit() {
     this.codeselect = JSON.parse(COOKIE.get('codeselect')).code;
@@ -34,14 +38,18 @@ export class EditcodeComponent implements OnInit {
     this.textarea.selectText(newCodeSelect);
   }
   ngAfterViewInit() {
+    this.code= JSON.parse(COOKIE.get('codeselect'));
     this.logger.getUser(JSON.parse(COOKIE.get('currentUser')).userId).subscribe((data) => this.user = data);
+    this.codeService.getCode(this.code.codeId).subscribe((d) => {
+      this.tagspost = d.tags;
+      this.langagepost = d.langage.langage;
+    });
   }
   /*save after edition*/
   saveCode(code:string){
     if(code === this.codeselect){
       alert('le code n\'a pas changé')
     }else{
-
       this.code= JSON.parse(COOKIE.get('codeselect'));
       this.codei.code = this.code.code;
       for(let tag of this.code.tags){
